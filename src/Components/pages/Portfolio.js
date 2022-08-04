6ee38bc79f0416555e750c54dd3162321262097c
DukeHdez <briayan27@outlook.com>
2022-08-04 11:27:11 -0400

    Add portfolio projects

---
+++ src/Components/pages/Portfolio.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Portfolio() {
+    return (
+        <section class="portfolio" id="portfolio">
+        <h2>Portfolio</h2>
+        <div class="portfolio-works">
+            <a href="https://lissygonz.github.io/bored-and-hungry/"><button id="boredandhungry"></button></a>
+            <div class="border-divison"></div>
+            <button>Project 2</button>
+            <div class="border-divison"></div>
+            <button>Project 3</button>
+            <div class="border-divison"></div>
+            <button>Project 4</button>
+            <div class="border-divison"></div>
+            <button>Project 5</button>
+        </div>
+    </section>
+    )
+}
