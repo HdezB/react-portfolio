c2910a8b85573b06c43ffb81d7ce8e9451098894
DukeHdez <briayan27@outlook.com>
2022-08-04 10:58:48 -0400

    Add about page

---
+++ src/Components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section class="about" id="about">
+            <div>
+                <h2>Hello There! I'm Brian.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsa animi soluta nam quia, voluptate molestias itaque adipisci quam temporibus qui amet error totam
+                    optio sit fugit corrupti,
+                    tempora inventore eaque.
+                </p>
+                <div class="image-icon"></div>
+            </div>
+        </section>
+    )
+}
